e62c9088ce5e7c7954d0df2c7fc1553945736cf5
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2023-10-05 06:09:49 +0000

    Added comments to the math example.

--- math_api.py
+++ math_api.py
@@ -2,12 +2,26 @@ from typing import Protocol
 
 class API(Protocol):
     def add(self, x: float, y: float) -> float:
+        """Add two numbers"""
         ...
     def sub(self, x: float, y: float) -> float:
+        """Subtract two numbers"""
         ...
     def mul(self, x: float, y: float) -> float:
+        """Multiple two numbers"""
         ...
     def div(self, x: float, y: float) -> float:
+        """Divide two numbers"""
         ...
-    def exp(self, base: float, pow: float) -> float:
+    def pow(self, base: float, exp: float) -> float:
+        """Raise a base to a power."""
+        ...
+    def neg(self, x: float) -> float:
+        """Negate a number"""
+        ...
+    def id(self, x: float) -> float:
+        """Identity function"""
+        ...
+    def unknown(self, message: str) -> float:
+        """Unknown request"""
         ...
